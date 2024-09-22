00f2d73ad62927b7a5490babdf691b39516e3849
Susana Becerra Pino <susanabp1992@gmail.com>
2024-09-22 19:42:44 -0400

    Edit run command again

--- HelloWorldApp/obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ HelloWorldApp/obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d260f1dd69c7b617261eb4545af379752f87bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1b38344d846175046c01936f086e10cf3e1d40")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
