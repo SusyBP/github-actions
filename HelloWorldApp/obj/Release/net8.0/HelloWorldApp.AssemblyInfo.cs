3f1b38344d846175046c01936f086e10cf3e1d40
Susana Becerra Pino <susanabp1992@gmail.com>
2024-09-22 19:34:00 -0400

    Edit run command

--- HelloWorldApp/obj/Release/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ HelloWorldApp/obj/Release/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d260f1dd69c7b617261eb4545af379752f87bcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
